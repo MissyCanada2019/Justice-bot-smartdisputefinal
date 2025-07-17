f0ec83ef2048b91e9f5728c8d773ceb6b738ffb9
MissyCanada2019 <terri.bertin57@gmail.com>
2025-07-17 16:39:27 -0400

    COMMIT MESSAGE

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/user/studio/app/page.tsx
+// File: C:\Users\Admin\justice-bot.com\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
